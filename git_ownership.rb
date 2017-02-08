1675ce83830909e275d504dce10afcdbd50fd848
aliciasavelly <alicia.savelly@gmail.com>
2017-02-07 18:03:15 -0800

    fix it

--- git_ownership.rb
+++ git_ownership.rb
@@ -1,1 +1,2 @@
+Fix git ownership
 git filter-branch -f --env-filter "GIT_AUTHOR_NAME='aliciasavelly'; GIT_AUTHOR_EMAIL='alicia.savelly@gmail.com'; GIT_COMMITTER_NAME='aliciasavelly'; GIT_COMMITTER_EMAIL='alicia.savelly@gmail.com';" HEAD
